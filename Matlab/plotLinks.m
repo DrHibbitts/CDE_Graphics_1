bebf9fc168b27fd8ec389bbced034d4c50afc7b5
DrHibbitts <drhibbitts@googlemail.com>
2014-11-17 11:42:07 +0000

    Added Support for 3d

--- Matlab/plotLinks.m
+++ Matlab/plotLinks.m
@@ -1,13 +1,16 @@
 function plotLinks(X, r, aSize)
 % Plot the results
 
-plot(X(1,:),X(2,:),'O'); hold on;
-plot(X(1,:),X(2,:),'-','Linewidth',3);
+plot3(X(1,:),X(2,:),X(3,:),'O'); hold on;
+plot3(X(1,:),X(2,:),X(3,:),'-','Linewidth',3);
 ang = 0:(2*pi/360):2*pi;
 x = X(1,1)+r*cos(ang);
 y = X(2,1)+r*sin(ang);
-plot(x,y);
+plot3(x,y,zeros(size(x)));
+plot3(x,zeros(size(x)),y);
+plot3(zeros(size(x)),x,y);
 axis(aSize); axis equal;
+view(2);
 hold off;
 
 end
